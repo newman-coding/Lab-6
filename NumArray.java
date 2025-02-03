82a4ad2310755009d2b99e1704437cec9f848f34
Sav Bowerfind <savbowerfind@newmanschool.org>
2025-02-03 15:38:34 -0600

    Updated starter code

---
+++ NumArray.java
@@ -0,0 +1,49 @@
+public class NumArray {
+
+    /*
+     * Returns the sum of a given integer array.
+     * Preconditions: a must be initialized
+     * @param a Integer array to be summed
+     * @return sum of array a
+     */
+    public static int sumUp(int[] arr){
+
+    }
+
+    public static int underTen(double[] arr){
+
+    }
+
+    public static int[] scalarMult(int[] arr, int scalar){ 
+
+    }
+
+    public static int[] vectorAdd(int[] arr1, int[] arr2){ 
+
+    }
+
+    public static int[] vectorSubtract(int[] arr1, int[] arr2){ 
+
+    }
+
+    public static int dotProduct(int[] arr1, int[] arr2){ 
+
+    }
+
+    public static int minArray(int[] arr){ 
+
+    }
+
+    public static void printArray(int[] arr){ 
+
+    }
+
+    public static int randomEntry(int[] arr){ 
+
+    }
+
+    public static int[] subArray(int[] nums, int start, int end){
+
+    }
+    
+}
